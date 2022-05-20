efb86535014e7b0c16deeeb2831247f51ee00964
Yuriy Hrytsaiy <yu.hritsaiy@gmail.com>
2022-05-20 09:22:09 +0300

    init

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,7 +22,7 @@ if [[ $BODY == *".take"* ]]; then
 fi
 
 echo "Using the link: $URL"
-curl -H "Authorization: token $GITHUB_TOKEN" -d '{"body": "Great stuff!","commit_id": "'$GITHUB_SHA'","path": "app/Http/Controllers/Controller.php","start_line": 13,"start_side": "RIGHT","line": 16,"side": "RIGHT"}' $URL
+curl -H "Authorization: token $GITHUB_TOKEN" -d '{"body": "Great stuff!","commit_id": "'"$GITHUB_SHA"'","path": "app/Http/Controllers/Controller.php","start_line": 13,"start_side": "RIGHT","line": 16,"side": "RIGHT"}' $URL
 
 #curl --location --request POST "https://api.github.com/repos/$REPO/pulls/$PR_NUMBER/comments" \
 #--header 'Accept: application/vnd.github.v3+json' \
